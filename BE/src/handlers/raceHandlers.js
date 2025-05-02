07d29a96c98c2eab95c232b931588dc53afb383e
SimonAppel <simon.appel@usm.cl>
2025-05-02 18:44:44 -0400

    chore: adding backend with test

---
+++ BE/src/handlers/raceHandlers.js
@@ -0,0 +1,15 @@
+const { RaceModel } = require('../model/race');
+
+const getAllRaces = () => RaceModel.find();
+const createRace = (data) => new RaceModel(data).save();
+const getRaceById = (id) => RaceModel.findById(id);
+const updateRace = (id, data) => RaceModel.findByIdAndUpdate(id, data, { new: true });
+const deleteRace = (id) => RaceModel.findByIdAndDelete(id);
+
+module.exports = {
+  getAllRaces,
+  createRace,
+  getRaceById,
+  updateRace,
+  deleteRace
+};
