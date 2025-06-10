e783b4abe593c385b73a46b772975fba022178c7
JoseGuerrero16 <103549532+JoseGuerrero16@users.noreply.github.com>
2025-06-10 14:22:55 -0400

    Add files via upload

--- BE/src/handlers/raceHandlers.js
+++ BE/src/handlers/raceHandlers.js
@@ -1,6 +1,6 @@
 const { RaceModel } = require('../model/race');
 
-const getAllRaces = () => RaceModel.find();
+const getAllRaces = (query = {}) => RaceModel.find(query);
 const createRace = (data) => new RaceModel(data).save();
 const getRaceById = (id) => RaceModel.findById(id);
 const updateRace = (id, data) => RaceModel.findByIdAndUpdate(id, data, { new: true });
--- BE/src/routes/raceRoutes.js
+++ BE/src/routes/raceRoutes.js
@@ -3,11 +3,22 @@ const router = express.Router();
 const raceData = require('../handlers/raceHandlers');
 
 router.get('/data', async (req, res) => {
-  const races = await raceData.getAllRaces();
-  console.log(JSON.stringify(races));
+  const { name, location, dateFrom, dateTo } = req.query;
+  const query = {};
+  
+  if (name) query.name = new RegExp(name, 'i');
+  if (location) query.location = new RegExp(location, 'i');
+  if (dateFrom || dateTo) {
+    query.date = {};
+    if (dateFrom) query.date.$gte = dateFrom;
+    if (dateTo) query.date.$lte = dateTo;
+  }
+
+  const races = await raceData.getAllRaces(query);
   res.json(races);
 });
 
+// Los demás endpoints permanecen igual
 router.get('/data/:id', async (req, res) => {
   const race = await raceData.getRaceById(req.params.id);
   race ? res.json(race) : res.status(404).send('Not found');
