9fe554a2232b4d1983d1b0a5cd1e95fcf7d75b31
Bastian Gaete <bastian.gaete01@gmail.com>
2025-07-02 23:19:39 -0400

    Fix tests and deploy

--- FE/tests/test_login_calif.py
+++ FE/tests/test_login_calif.py
@@ -16,7 +16,7 @@ class TestLogin:
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        options.binary_location = "/usr/bin/google-chrome"
+        options.binary_location = "/snap/bin/chromium"
         self.driver = webdriver.Chrome(options=options)
         self.driver.set_window_size(1920, 1080)
 
